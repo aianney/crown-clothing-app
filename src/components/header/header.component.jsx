72dd71101dcf975819e756aabce2f092515a65f3
yannie4163 <73107567+aianney@users.noreply.github.com>
2021-08-04 15:29:32 +0800

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import CartIcon from '../cart-icon/cart.icon.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart/cart-dropdown.component';
 
 const Header = ({ currentUser, hidden }) => (
