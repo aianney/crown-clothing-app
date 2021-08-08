0dcb7848bbe499339d14ad88a265337cd9874665
yannie4163 <73107567+aianney@users.noreply.github.com>
2021-08-08 20:52:17 +0800

    created user and cart sagas for sign in, sign up and sign in with goggle

--- src/components/stripe-button/stripe.button.component.jsx
+++ src/components/stripe-button/stripe.button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const pulishablekey = 'pk_test_51J0MNmKzWetS0rOLUUKX4Ein4hC3Dn9EpVI0fP3vq4h4orVmR5qScipQh8mXDS6MM2ZFL6GklgMZidXSRh26OTaY00iVqEDiCn'
+    const pulishablekey = 'pk_test_51JL0dwKjLm3nHsPePfOnFxcHO9quGbg2zdqM4AYluyrSLhrTqdSNjk9Xd31OUCL1R4exdM3tGa0JMueiLEWIyaGL00O1gMTGna'
 
 
   const onToken = token => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+import createSagaMiddleware from 'redux-saga';
+
 
 import rootReducer from "./root-reducer";
+import rootSaga from './root-saga';
 
-const middlewares = [];
+const sagaMidlleware = createSagaMiddleware();
+
+const middlewares = [sagaMidlleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMidlleware.run(rootSaga);
 
 export const persistor = persistStore(store);
